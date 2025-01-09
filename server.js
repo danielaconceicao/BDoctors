8dabf5bca38841c069b7591b1cbdac1ab24729d8
IulianBotnari <iulianbotnari93@gmail.com>
2025-01-09 10:16:27 +0100

    [FIX] server host address

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const server = express();
 
-const host = process.env.HOST || "localhost"
+const host = process.env.HOST || "http://localhost"
 const port = process.env.PORT || 3000
 
 server.use(cors())
