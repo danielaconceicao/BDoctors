371b773e17573f7744fb4dfc75dcacf5b2631b7d
feffe1702 <fpm.asd@gmail.com>
2025-01-09 12:05:49 +0100

    [FIX] fix server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const server = express();
+const router = require("./routes/routes")
 
 const host = process.env.HOST || "http://localhost"
 const port = process.env.PORT || 3000
@@ -11,6 +12,8 @@ server.use(express.json())
 server.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to the server!" })
 })
+
+server.use("/", router)
 
 server.listen(port, (req, res) => {
     console.log(`Server running on ${host}:${port}`)
