f8e0593f11ee8e70213fb52506afd4111a9706b6
IulianBotnari <iulianbotnari93@gmail.com>
2025-01-09 12:08:07 +0100

    [FIX] Db connection

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors')
-const server = express();
+const server = express()
+const env = require('dotenv').config()
 
 const host = process.env.HOST || "http://localhost"
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3004
 
 server.use(cors())
 server.use(express.json())
+
+const router = require('./routes/routes.js')
+
+server.get('/', router)
 
 server.listen(port, (req, res) => {
     console.log(`Server running on ${host}:${port}`)
 
 })
+
