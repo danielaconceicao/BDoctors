40e5deacba30032d136a41530c7efe05fde00c82
IulianBotnari <iulianbotnari93@gmail.com>
2025-01-09 10:10:51 +0100

    Fix cors module

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const server = express();
 
-server.use(cors())
 const host = process.env.HOST || "localhost"
 const port = process.env.PORT || 3000
 
+server.use(cors())
 server.use(express.json())
 
 server.listen(port, (req, res) => {
