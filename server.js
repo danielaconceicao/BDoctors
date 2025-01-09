b9c87a25dac5351508775076e00a4e45b04fdd0b
feffe1702 <fpm.asd@gmail.com>
2025-01-09 12:00:01 +0100

    [FIX] server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3000
 server.use(cors())
 server.use(express.json())
 
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the server!" })
+})
+
 server.listen(port, (req, res) => {
     console.log(`Server running on ${host}:${port}`)
 
