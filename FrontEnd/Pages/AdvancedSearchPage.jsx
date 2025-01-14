f5e011e3fc0d8ccdc405731975d3c012ce54a9a1
IulianBotnari <iulianbotnari93@gmail.com>
2025-01-14 09:43:11 +0100

    [WIP] Add bootstrap and bootstrap icons module, add responsive class for doctors card

--- FrontEnd/Pages/AdvancedSearchPage.jsx
+++ FrontEnd/Pages/AdvancedSearchPage.jsx
@@ -8,11 +8,12 @@ export default function AdvancedSearchPage() {
 
     return (
         <>
-            <div className={`${style.doctor} ${style.mx}`}>
+            <div className={`container-sm container-md container-lg container-xl container-xxl ${style.doctor} `}>
                 <img src="https://picsum.photos/60/90" alt="ProfileImg"></img>
                 <p>Nome: <span>Paolo</span></p>
                 <p>Cognome: <span>Rossi</span></p>
                 <p>Email: <span>doctor@email.com</span></p>
+                <p>Tel: <span>3479875828</span></p>
                 <p>Indirizzo: <span>Via Garibaldi civico 3, Trieste </span></p>
                 <p>Specializzazione: <span>Neurochirurgo</span></p>
 
