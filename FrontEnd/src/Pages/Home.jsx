61dd2717123d3dac3d5a261fb543148380652b60
feffe1702 <fpm.asd@gmail.com>
2025-01-16 15:03:36 +0100

    [FIX] homepage

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -1,40 +1,30 @@
 import { useGlobalContext } from "../Context/GlobalContext";
 import { useTranslation } from "react-i18next";
 import "../i118";
-import { useState } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 import FeaturedDoctorsCards from "../Components/FeaturedDoctorsCards";
 
 export default function Home() {
-    const { specializations, doctors, setFilteredDoctors, loading, error, setSelectedSpecialization } = useGlobalContext();
+    const { specializations, loading, error } = useGlobalContext();
     const { t } = useTranslation();
-
     const navigate = useNavigate();
 
-
-    const handleCardClick = (e) => {
-        const specializationName = e.currentTarget.getAttribute('data-specialization-name');
-        setSelectedSpecialization(specializationName);
-
-        // Filtra i dottori in base alla specializzazione selezionata
-        const filtered = doctors.filter(doctor => {
-            return doctor.specializations.split(',').includes(specializationName);
-        });
-
-        setFilteredDoctors(filtered); // Aggiorna lo stato con i dottori filtrati
-        // console.log('Dottori filtrati:', filtered);
-        navigate('/AdvancedSearchPage'); // Naviga alla pagina dei dottori filtrati
+    const handleCardClick = (specializationName) => {
+        // Naviga alla pagina AdvancedSearchPage con la specializzazione nella rotta
+        navigate(`/AdvancedSearchPage/${specializationName}`);
     };
 
     return (
         <div>
-
             <div className="container">
                 <div className="d-flex justify-content-between">
                     <h1>Specializzazioni</h1>
-                    <NavLink to="/DoctorRegistration" className="register_btn d-none d-md-block">SEI UN DOTTORE? REGISTRATI QUI</NavLink>
-                    <NavLink to="/DoctorRegistration" className="register_btn d-md-none"><i className="bi bi-plus-circle"></i></NavLink>
-
+                    <NavLink to="/DoctorRegistration" className="register_btn d-none d-md-block">
+                        SEI UN DOTTORE? REGISTRATI QUI
+                    </NavLink>
+                    <NavLink to="/DoctorRegistration" className="register_btn d-md-none">
+                        <i className="bi bi-plus-circle"></i>
+                    </NavLink>
                 </div>
                 <div className="row">
                     {loading && <p>Caricamento in corso...</p>}
@@ -46,26 +36,21 @@ export default function Home() {
 
                     {!loading && !error && specializations.length > 0 && (
                         <div className="cards-container">
-                            {specializations.map(specialization => (
+                            {specializations.map((specialization) => (
                                 <div
                                     className="card_custom"
                                     key={specialization.id}
-                                    data-specialization-name={specialization.specialization_name}
-                                    onClick={handleCardClick} // Aggiunto evento click
+                                    onClick={() => handleCardClick(specialization.specialization_name)}
                                 >
                                     <h3>{t(specialization.specialization_name)}</h3>
-                                    {/* <p>{specialization.description}</p> */}
                                 </div>
                             ))}
                         </div>
                     )}
-
-
                 </div>
-                <h2 className="mt-3 mb-3">I Nostri Dottori in Evidenza</h2>
+                <h2 className="mt-3 mb-3">I Nostri 5 Dottori in Evidenza</h2>
                 <FeaturedDoctorsCards />
             </div>
-
         </div>
     );
 }
