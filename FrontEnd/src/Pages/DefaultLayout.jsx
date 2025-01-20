fdc132b6dc2443befe370aa3da0c18817b7640ef
feffe1702 <fpm.asd@gmail.com>
2025-01-20 11:21:17 +0100

    [WIP] add navlink to logo

--- FrontEnd/src/Pages/DefaultLayout.jsx
+++ FrontEnd/src/Pages/DefaultLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import BackButton from "../Components/BackButton";
 import style from "../styles/DefaultLayout.module.css"
+import { NavLink } from "react-router-dom";
 
 export default function DefaultLayout() {
     return (
@@ -8,7 +9,9 @@ export default function DefaultLayout() {
         <>
             <header className="d-flex justify-content-between align-items-center px-4 py-2 ">
                 <div className="d-flex align-items-center">
-                    <img className={style.logo} src="Logo.png" alt="" />
+                    <NavLink className={style.logo} to="/">
+                        <img src="Logo.png" alt="" />
+                    </NavLink>
                     <h1>BDoctors</h1>
 
                 </div>
